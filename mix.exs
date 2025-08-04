72b8be08e154a04468316a63e737d17f3fc778dc
ogabriel <gabrieloliver8991@gmail.com>
2025-08-03 22:19:58 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugCodeReloader.MixProject do
   def project do
     [
       app: :plug_code_reloader,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
