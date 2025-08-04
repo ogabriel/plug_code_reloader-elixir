1a95b04af0ffa8c330736f32d1c2dc7be25faad6
ogabriel <gabrieloliver8991@gmail.com>
2025-08-03 21:56:34 -0300

    chore: add ex_doc

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule PlugCodeReloader.MixProject do
       {:plug, "~> 1.18"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
